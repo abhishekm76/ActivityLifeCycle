c14f445479851550130def18b6d05ecd5b9ce2f6
Abhishek <abhishek.amishra@gmail.com>
2019-11-28 20:38:13 +0530

    FirstCommit

--- app/src/main/java/com/delloil/activitylifecycle/MainActivity.java
+++ app/src/main/java/com/delloil/activitylifecycle/MainActivity.java
@@ -2,17 +2,43 @@ package com.delloil.activitylifecycle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button showGuess;
+    private EditText userGuess;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         MakeToast("OnCreate() Called");
+        showGuess=findViewById(R.id.button_submitguess);
+        userGuess=findViewById(R.id.enter_guess);
+        showGuess.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String enteredText = userGuess.getText().toString().trim();
+
+                if (!enteredText.isEmpty()){
+                    Intent navIntent = new Intent(MainActivity.this, ShowGuess.class);
+                    navIntent.putExtra("guesskey",enteredText);
+                    startActivity(navIntent);
+                }else{
+                    Toast.makeText(MainActivity.this, "You need to enter a name",Toast.LENGTH_SHORT).show();
+
+                }
+
+            }
+        });
+
+
+
     }
 
     @Override
---
+++ app/src/main/java/com/delloil/activitylifecycle/ShowGuess.java
@@ -0,0 +1,23 @@
+package com.delloil.activitylifecycle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class ShowGuess extends AppCompatActivity {
+
+    private TextView showGuess;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_guess);
+        showGuess=findViewById(R.id.recievedText);
+        if (getIntent().getStringExtra("guesskey") != null){
+            String passedFromOne = getIntent().getStringExtra("guesskey");
+            showGuess.setText(passedFromOne);
+        }
+    }
+
+}
